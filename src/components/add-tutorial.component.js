8ebc95bea9a61d4153f80937428475247778edf9
iraisav <zs19004913@estudiantes.uv.mx>
2022-03-31 16:30:33 -0600

    adding routes

---
+++ src/components/add-tutorial.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class AddTutorial extends Component {
+    render() {
+        return (
+            <div className="container mt-3">
+                <h2>ADD</h2>
+            </div>  
+        );
+    }
+}
+export default AddTutorial; 
---
+++ src/components/tutorial-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class TutorialsList extends Component {
+    render() {
+        return (
+            <div className="container mt-3">
+                <h2>LIST</h2>
+            </div>  
+        );
+    }
+}
+export default TutorialsList; 
